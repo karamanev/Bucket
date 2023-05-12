1e1fde2772c0c76670ddc188bf6d2eaae1af4c12
Georgi Karamanev <karamanev@gmail.com>
2023-05-12 11:03:50 +0300

    Add basic path listing and uploading

--- bucket/src/Pages/Bucket/bucket.tsx
+++ bucket/src/Pages/Bucket/bucket.tsx
@@ -1,5 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AWS, { AWSError } from 'aws-sdk';
+import { ListObjectsOutput, Object as AA } from 'aws-sdk/clients/s3';
+
+// Credentials from the task
+// const S3_BUCKET = 'interview-task-g-karamanev';
+// const accessKeyId = 'AKIAZ5RCAHL6MI5DXKNY';
+// const secretAccessKey = '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';
+
+// Working credentials
+const S3_BUCKET = 'karamanev-test';
+const accessKeyId = 'AKIAR56MQRQJOYC3YGJL';
+const secretAccessKey = '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';
+
+const REGION = 'eu-central-1';
+
+AWS.config.update({
+  accessKeyId,
+  secretAccessKey
+});
+
+const myBucket = new AWS.S3({
+  params: { Bucket: S3_BUCKET },
+  region: REGION
+});
 
 export const Bucket = () => {
-  return <h1>Bucket</h1>;
+  const [progress, setProgress] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [files, setFiles] = useState<string[] | null>(null);
+
+  const handleFileInput = (e: any) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const uploadFile = (file: any) => {
+    const params = {
+      ACL: 'public-read',
+      Body: file,
+      Bucket: S3_BUCKET,
+      Key: `prefix/${file.name}`
+    };
+
+    myBucket
+      .putObject(params)
+      .on('httpUploadProgress', (evt) => {
+        setProgress(Math.round((evt.loaded / evt.total) * 100));
+      })
+      .send((err) => {
+        if (err) console.log(err);
+      });
+  };
+
+  function download() {
+    // myBucket.getObject().send((a, err) => {
+    //   console.log(a);
+    //   if (err) console.log(err);
+    // });
+
+    // myBucket.listBuckets().send((a, err) => {
+    //   console.log(a);
+    //   if (err) console.log(err);
+    // });
+
+    // myBucket.listObjects().send((a, err) => {
+    //   console.log(a);
+    //   if (err) console.log(err);
+    // });
+
+    const a = function (err: AWSError, data: ListObjectsOutput) {
+      console.log(err);
+      console.log(data);
+      if (data.Contents) {
+        console.log(data.Contents);
+
+        const a = data.Contents.map((a) => a.Key) as string[];
+
+        setFiles(a);
+      }
+    };
+
+    //    myBucket.listBuckets(a);
+
+    myBucket.listObjects(a);
+  }
+
+  return (
+    <div>
+      <div>File Upload Progress is {progress}%</div>
+      <input type="file" onChange={handleFileInput} />
+      <button onClick={() => uploadFile(selectedFile)}>
+        {' '}
+                Upload to S3
+      </button>
+      <button onClick={() => download()}> Download </button>;
+      <div>{files}</div>
+    </div>
+  );
 };
--- bucket/src/Pages/Login/login.tsx
+++ bucket/src/Pages/Login/login.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-
 export const Login = () => {
-  return (
-    <h1>Login</h1>
-  )
+  return <h1>Login</h1>;
 };
